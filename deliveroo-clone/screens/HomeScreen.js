a212e3880ccc7012e535c98d5596b05d1c34fb73
@Mikist <mikeochango3@gmail.com>
2024-03-14 23:08:36 +0300

    CREATED HOME SCREEN

--- deliveroo-clone/screens/HomeScreen.js
+++ deliveroo-clone/screens/HomeScreen.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 function HomeScreen() {
   return (
     <View>
-    <Text className="text-blue-600 font-semibold text-lg" >Home Screen from Mike</Text>
+    <Text className="text-blue-600 font-semibold text-lg" >Home  Screen from Mike</Text>
+    <Text className="text-blue-600 font-semibold text-lg" >Home  Screen from Mike</Text>
      </View>
   )
 }
