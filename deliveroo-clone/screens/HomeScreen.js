c270a538885dcf64b08e97619b692fc0ff6eab5f
@Mikist <mikeochango3@gmail.com>
2024-03-14 23:00:30 +0300

    First commit

---
+++ deliveroo-clone/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View>
+    <Text className="text-blue-600 font-semibold text-lg" >Home Screen from Mike</Text>
+     </View>
+  )
+}
+
+export default HomeScreen
