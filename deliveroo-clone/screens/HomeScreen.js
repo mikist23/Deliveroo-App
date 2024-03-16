559d3b16862912c67e3bbb7be115f023f47329d4
@Mikist <mikeochango3@gmail.com>
2024-03-17 02:23:40 +0300

    Updated home screen

--- deliveroo-clone/screens/HomeScreen.js
+++ deliveroo-clone/screens/HomeScreen.js
@@ -1,9 +1,21 @@
 import React, { useLayoutEffect } from "react";
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
-import headerIMG from '../images/header.png'
-import { SparklesIcon, ChevronDownIcon } from "react-native-heroicons/solid";
+import headerIMG from "../images/header.png";
+import {
+  SparklesIcon,
+  ChevronDownIcon,
+  UserIcon,
+} from "react-native-heroicons/solid";
+import SearchIcon from "react-native-vector-icons/AntDesign";
 
 function HomeScreen() {
   const navigation = useNavigation();
@@ -16,29 +28,47 @@ function HomeScreen() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="mt-5 p-3">
-        <Text className="text-blue-600 font-semibold text-lg">
-          
+      <SafeAreaView className="mt-9 pt-5 bg-white">
+        
           {/**Header */}
-          
-          <View className="flex flex-row items-center mx-4 space-x-2">
-            <Image
-            source={headerIMG}
-            className="h-7 w-7 bg-gray-300 rounded-full p-4"
-            />
-
-            <View >
-              <Text className="font-bold text-gray-400 textsm">
-                Deliver Now
-              </Text>
-              <Text className="font-bold text-xl items-center">
-                Current Location
-                <ChevronDownIcon  size={20} color={"#00CCBB"} />
-              </Text>
+          <View className="flex flex-row items-center justify-between mx-4 space-x-2">
+            <View className="flex flex-row items-center space-x-2">
+              <Image
+                source={headerIMG}
+                className="h-7 w-7 bg-gray-300 rounded-full p-4"
+              />
+              <View className="flex-1">
+                <Text className="font-bold text-gray-400 text-sm">
+                  Deliver Now
+                </Text>
+                <Text className="font-bold text-xl items-center">
+                  Current Location
+                  <ChevronDownIcon size={20} color={"#00CCBB"} />
+                </Text>
+              </View>
+              <Text>
+              <UserIcon size={35} color={"#00CCBB"} />
+            </Text>
             </View>
+           
           </View>
-          
-        </Text>
+
+          {/**Search */}
+          <View className="flex-row items-center space-x-2 pb-2 mx-4 mt-3">
+            <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+              <SearchIcon
+                name="search1"
+                size={20}
+                color="gray"
+              />
+              <TextInput
+                placeholder="Search "
+                keyboardType="default"
+              />
+            </View>
+            
+          </View>
+        
       </SafeAreaView>
     </SafeAreaProvider>
   );
